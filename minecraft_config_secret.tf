2f513380b8ec7686b2b7b2f2d0f746b4a8e6735e
berkayh27 <berkayhidiroglu@hotmail.com>
2021-01-11 19:40:06 +0000

    added jenkins secret

---
+++ minecraft_config_secret.tf
@@ -0,0 +1,21 @@
+  
+resource "kubernetes_secret" "minecraft_config" {
+  metadata {
+    name = "minecraft-config"
+    namespace = "tools"
+
+    labels = {
+      "jenkins.io/credentials-type" = "secretFile"
+      
+    }
+
+    annotations = {
+      "jenkins.io/credentials-description" = "minecraft config"
+    }
+  }
+  
+  data = {
+    filename = "minecraft.tfvars"
+    "data" =  "${file("minecraft.tfvars")}"
+  }
+}
